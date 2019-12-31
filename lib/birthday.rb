e1ddb011b1adfe33bf93e0f4fae2407b68d23d56
Brianna Jordan <bryvjordan9@gmail.com>
2019-12-31 17:34:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,5 @@
 # }
 
 def happy_birthday(birthday_kids)
-  biirthday_kids.select {|age| age == 12 p "Happy 12th Birthday!"}
+  biirthday_kids.select {|age| age == 12 print "Happy 12th Birthday!"}
 end
