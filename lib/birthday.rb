636b72d269c45b9c7013e1c018c41b2f496f3644
Brianna Jordan <bryvjordan9@gmail.com>
2019-12-31 17:58:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,6 +9,14 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
-
-
 end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age <= 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else
+      puts "Happy Birthday #{name}! You are getting old!"
+    end
+  end
+end 
